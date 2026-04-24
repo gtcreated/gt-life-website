a8b4c6859f20d2bdbc0e480dce01139fb31500e1
Dhruv <dhruvvguptaa@gmail.com>
2026-04-24 05:49:59 +0530

    made changes to fix PWA

--- GT-Life-Website/sw.js
+++ GT-Life-Website/sw.js
@@ -1,9 +1,16 @@
-// This is a basic Service Worker to pass the PWA install requirements
+const CACHE_NAME = 'gt-life-cache-v1';
+
+// Install the Service Worker
 self.addEventListener('install', (event) => {
+    self.skipWaiting();
     console.log('GT Life App Installed!');
 });
 
+// Intercept network requests (satisfies Android Chrome's strict PWA rules)
 self.addEventListener('fetch', (event) => {
-    // This allows the app to fetch things from the internet normally
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
 });
